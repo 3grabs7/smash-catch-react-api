43b2c5a32a583e88b8cfc0d9dee83c05e974e382
Robin Grabs <robinmgrabs@gmail.com>
2021-11-26 22:58:31 +0100

    refactor: implement async call to seed from main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SeedGameEntities } from './GameSeed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await SeedGameEntities();
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
