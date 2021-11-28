50a09f3406c5226d9016d519d472082f2a0c0914
Robin Grabs <robinmgrabs@gmail.com>
2021-11-28 19:35:57 +0100

    feature: graphql annotations for player entity

--- src/player/entities/player.entity.ts
+++ src/player/entities/player.entity.ts
@@ -1,3 +1,4 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { GameResultEntity } from 'src/game-result/entities/game-result.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
@@ -10,25 +11,32 @@ export enum Level {
 }
 
 @Entity('player')
+@ObjectType('player')
 export class PlayerEntity {
   @PrimaryGeneratedColumn()
+  @Field(() => Int)
   id: number;
 
   @Column()
+  @Field()
   name: string;
 
   @Column({ unique: true })
+  @Field()
   userName: string;
 
   @Column({ default: 0 })
+  @Field(() => Int, { defaultValue: 0 })
   totalScore: number;
 
   @Column({ type: 'enum', enum: Level, default: Level.NOOB })
+  @Field({ defaultValue: Level.NOOB })
   profileLevel: Level;
 
   @OneToMany(
     () => GameResultEntity,
     (gameResultEntity) => gameResultEntity.player,
   )
+  @Field(() => [GameResultEntity], { nullable: true })
   results: GameResultEntity[];
 }
