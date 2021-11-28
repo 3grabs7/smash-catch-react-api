baaa731e1b9759d7c1d05f66bc800650fa8e35d2
Robin Grabs <robinmgrabs@gmail.com>
2021-11-28 19:43:17 +0100

    feature: graphql notations for game result entity

--- src/game-result/entities/game-result.entity.ts
+++ src/game-result/entities/game-result.entity.ts
@@ -1,3 +1,4 @@
+import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
 import { PlayerEntity } from 'src/player/entities/player.entity';
 import {
   Entity,
@@ -10,18 +11,29 @@ import {
 import { GameEntity } from '../../game/entities/game.entity';
 
 @Entity('game_result')
+@ObjectType()
 export class GameResultEntity {
   @PrimaryGeneratedColumn()
+  @Field(() => Int)
   id: number;
 
   @Column()
+  @Field()
   score: string;
 
   @ManyToOne(() => GameEntity, (gameEntity) => gameEntity.results)
+  @Field(() => GameEntity)
   game: GameEntity;
 
+  @Column()
+  @Field(() => Int)
+  gameId: number;
 
   @ManyToOne(() => PlayerEntity, (playerEntity) => playerEntity.results)
+  @Field(() => PlayerEntity)
   player: PlayerEntity;
 
+  @Column()
+  @Field(() => Int)
+  playerId: number;
 }
